4798ee8b1a5b2c90129e3ab6f87ab8008f6cfd9e
ＭＡＫＴＳＺＫＥＩ <mtszkei@gmail.com>
2024-11-06 11:49:34 +0800

    migration and seed updated

--- nextjs/migrations/20241103234913_users.js
+++ nextjs/migrations/20241103234913_users.js
@@ -5,7 +5,7 @@
 exports.up = async function (knex) {
     await knex.schema.createTable('users', table => {
         table.increments('id').primary()
-        table.string('username', 255).unique().notNullable()
+        table.string('nickname', 255).unique().notNullable()
         table.string('password', 255).notNullable()
         table.string('phone', 255).notNullable()
         table.boolean('admin').defaultTo(false)
