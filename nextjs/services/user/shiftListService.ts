c9edf01b5cb3af2db70b8e049b6718e81db58d46
Michael Yip <micmicyip@hotmail.com>
2024-11-19 18:02:55 +0800

    update shift list

--- nextjs/services/user/shiftListService.ts
+++ nextjs/services/user/shiftListService.ts
@@ -3,24 +3,26 @@ import { pgClient } from "@/lib/pgClient";
 class MobileShiftListService {
   constructor() {}
 
-  async getShiftListById (id: string) {
+  async getShiftListById(id: string) {
     try {
       const sql = `
       SELECT 
       shifts.user_id as user_id,
       shifts.date as date,
-      shift_slots.title as shift_slot
+      shift_slots.title as shift_slot,
+       shift_slots.start_time as start_time,
+      shift_slots.end_time as end_time
     FROM shifts
     JOIN shift_slots ON shifts.shift_slot_id = shift_slots.id
     WHERE shifts.user_id = $1
     ORDER BY
     shifts.date ASC
-    `
-    const result = await pgClient.query(sql, [id])
+    `;
+      const result = await pgClient.query(sql, [id]);
 
-    return result.rows
+      return result.rows;
     } catch (error) {
-      console.log("cannot get employees:", error)
+      console.log("cannot get employees:", error);
       throw new Error("Failed to retrieve employees");
     }
   }
